e33d9c056cec21c8fef684913ec1719a64a93ea6
sadimohammad7@gmail.com <sadimohammad7@gmail.com>
2015-11-19 01:12:33 +0600

    Initial Commit

---
+++ app.js
@@ -0,0 +1,62 @@
+
+
+var express = require('express'),
+    app = express(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server),
+    mongoose = require('mongoose') ;
+
+
+
+//cache session name
+var session = [];
+
+
+// environments
+app.set('views', __dirname+ '/views');
+app.use(express.static(__dirname + '/public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+//routing
+app.get('/', function(req,res){
+    res.render('home');
+});
+
+
+//Initialize a new socket.io application,
+io.sockets.on('connection', function(socket){
+
+    //savings login information
+    socket.on('login',function(data, callback){
+        if(session.length > 0 ){
+            callback(false);
+        }else{
+            callback(true);
+        }
+        // Use the socket object to store data.
+        socket.username= data;
+
+    });
+
+    //savings session information
+    socket.on('session',function(data){
+        // Use the socket object to store data.
+        socket.sessionname= data;
+        session.push(socket.sessionname);
+    });
+
+    // Handle the sending of messages
+    socket.on('msg', function(data){
+
+        // When the server receives a message,
+        // it sends it to the other person in the session.
+         socket.broadcast.emit('receive', {msg: data.msg, user: data.user});
+
+    });
+});
+
+
+server.listen(3000,function(){
+    console.log('listening on *:3000');
+});
---
+++ public/js/main.js
@@ -0,0 +1,139 @@
+$(document).ready(function() {
+
+    var socket = io.connect(),
+        messageTimeSent = $(".timesent"),
+        chat = $(".chats"),
+        textarea = $("#messages");
+
+    // variables which hold the data for each person
+    var name = "",
+        session = "";
+    var scroll = 0;
+
+
+
+    //form
+    var yourname= $('.yourname'),
+        yoursession= $('.yoursession');
+
+    //login information send to the server
+    $('.login_form').submit(function(e){
+        e.preventDefault();
+        name= $.trim(yourname.val());
+        if(name.length < 1){
+            alert("Please enter a nick name longer than 1 character!");
+            return;
+        }
+        $('.login_area').css('display', 'none');
+        $('.afterlogin').fadeIn(1500);
+        //sending user name to the server
+        socket.emit("login", name,  function(data) {
+            if (data) {
+                $('.chatscreen').fadeOut(1500);
+                $('.message_area').css('display', 'none');
+            } else {
+                $('.session_area').fadeOut(1500);
+            }
+        });
+        yourname.val('');
+        return false;
+    });
+
+
+    //session information send to the server
+    $('.session_form').submit(function(e){
+        e.preventDefault();
+        session= $.trim(yoursession.val());
+        if(session.length < 1){
+            alert("Please enter a nick name longer than 1 character!");
+            return;
+        }
+        $('.session_area').fadeOut(1500);
+        $('.chatscreen').fadeIn(1500);
+        $('.message_area').fadeIn(1500);
+        socket.emit('session' ,session)
+        yoursession.val('');
+        return false;
+
+    });
+
+
+    $("textarea").keydown(function(e){
+        if (event.keyCode == 13) {
+            e.preventDefault();
+            $('.message_form').submit();
+
+        }
+    });
+
+    //a new chat message and display it
+    $('.message_form').submit( function(e){
+        e.preventDefault();
+        if(textarea.val().trim().length){
+            createChatMessage(textarea.val(),name,moment());
+            scrollToBottom();
+            // Send the message to the other person in the chat
+            socket.emit('msg', {msg: textarea.val(), user: name});
+        }
+        textarea.val(" ");
+        return false;
+    });
+
+    socket.on('receive', function(data){
+        if(data.msg.trim().length) {
+            createChatMessage(data.msg, data.user, moment());
+            scrollToBottom();
+        }
+    });
+
+    // Update the relative time stamps on the chat messages every minute
+    setInterval(function(){
+
+        messageTimeSent.each(function(){
+            var each = moment($(this).data('time'));
+            $(this).text(each.fromNow());
+        });
+
+    },60000);
+
+    // Function that creates a new chat message
+    function createChatMessage(msg, user, now){
+
+        var who = '';
+
+        if(user===name) {
+            who = 'me';
+        }
+        else {
+            who = 'other';
+        }
+
+        var li = $(
+            '<li class=' + who + '>'+
+            '<div class="image">' +
+            '<i class="fa fa-user fa-4" style="color:red"></i>'+
+            '<b></b>' +
+            '<i class="timesent" data-time=' + now + '></i> ' +
+            '</div>' +
+            '<p></p>' +
+            '</li>');
+
+        // use the 'text' method to escape malicious user input
+        li.find('p').text(msg);
+        li.find('b').text(user);
+
+        chat.append(li);
+
+        messageTimeSent = $(".timesent");
+        messageTimeSent.last().text(now.fromNow());
+    }
+
+    function scrollToBottom(){
+        scroll = scroll+150;
+        console.log(scroll);
+        $(".chatscreen").animate({ scrollTop: scroll},1000);
+
+    }
+});
+
+
